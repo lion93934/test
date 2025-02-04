f70f430de7c4d53a7dacd111fc049c43c75c1b5c
lion93934 <boredapeigreen4@outlook.com>
2025-02-04 05:37:46 -0800

    Create kill.ps1

---
+++ kill.ps1
@@ -0,0 +1,9 @@
+ewewdjeon3i4o44idkknwejdnkilldqwwedejdndjwekdwcjefkvnihrwvbtwrrvkndjsnjc3ie-84-49434933=914[04r3=r823f594343441DU*@M(U<)#!*
+U#!(#@<US#S(@*_JS#*(_#<K##D<D43j89zd48x 1-j8f4hf-934)@)_!S(!@(S()!()#_))D$-0>?H&>%)Hedd edkjsdcdscdsjswiosj398823d7d3
+dje902wo22o1w!A!O_K_0001-w9300dkid039ek3ek09ks0ks293kKDK(LS)2s-l)3s3ikkkk2doepdkK__D_#OD\04do-4koqdkpdpkkopk.dzd[ldc.pdk
+ddep3.[l;,;/in,;lo[,lolpokjiptiotvtvuncdhkex2ijo43mjxjrefrfrnjgnjnjzjkrfefk92w0hs78sh7brhryhf7fhj48fhj4fj8fjf89opspwodo
+eijwiod843498dj389wi90239e3fur985hf54739i5j9858umc**M#U*SM39HHMSH#SI)))))##S)(S(#()(S#J#(SJ#J*#J(*J#J**#J*#)@(#2JU(#(S)#S
+834984r98jur48rd340i49iri4954iotkifiojioIXIOJJIOEIJIXkeixoiecnrevnturitvntnvskdddcm349r4344rjio(((((djdjdjjdjdsmsm*******
+poesem43o4dmn3dsi4d_K_0001-w9300dkid039ek3ek09ks0ks293kKDK(LS)2s-l)3s3ikkkk2doepdkK__D_#OD\04do-4koqdkpdpkkopk.dzd[ldc.pdk
+ddep3.[l;,;/in,;lo[,lolpokjiptiotvtvuncdhkex2ijo43mjxjrefrfrnjgnjnjzjkrfefk92w0hs78sh7brhryhf7fhj48fhj4fj8fjf89opspwodo
+eijwiod843498dj389wi90239e3fur985hf54739i5j9858umc**M#U*SM39HHMSH#SI)))))##S)(S(#()(S#J#(SJ#J*#J(*J#J**#J*#)@(#2JU(#(S)#S
